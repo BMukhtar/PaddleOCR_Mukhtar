d6b20e61704a3f7cb137793edaa90e1156ef2376
Mukhtar Bimurat <bimurat.mukhtar@gmail.com>
2023-09-01 15:29:32 +0600

    revert paddleocr.py changes

--- paddleocr.py
+++ paddleocr.py
@@ -60,7 +60,7 @@ __all__ = [
 
 SUPPORT_DET_MODEL = ['DB']
 VERSION = '2.7.0.1'
-SUPPORT_REC_MODEL = ['CRNN', 'SVTR_LCNet', "SVTR"]
+SUPPORT_REC_MODEL = ['CRNN', 'SVTR_LCNet']
 BASE_DIR = os.path.expanduser("~/.paddleocr/")
 
 DEFAULT_OCR_MODEL_VERSION = 'PP-OCRv4'
